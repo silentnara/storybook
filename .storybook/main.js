32bd57f18507d7e685afd25a9cd353b99c3d4014
silentnara <31914090+silentnara@users.noreply.github.com>
2020-08-02 19:51:06 +0900

    fix: start error

---
+++ .storybook/main.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.(js|mdx|tsx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-docs/preset',
+  ],
+
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [['react-app', { flow: false, typescript: true }]],
+            plugins: [
+              [
+                require.resolve('babel-plugin-named-asset-import'),
+                {
+                  loaderMap: {
+                    svg: {
+                      ReactComponent: '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+                    },
+                  },
+                },
+              ],
+            ],
+          },
+        },
+        require.resolve('react-docgen-typescript-loader'),
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
